2f82235e37dd97d01fd25fae78efd78d37ae00db
teegoood <teegooodgood@gmail.com>
2024-01-05 13:09:47 +0700

    deploy: vercel setup

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors')
+const dotenv = require('dotenv');
+dotenv.config();
 
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1/course-dashboard';
 const PORT = process.env.PORT || 9000 ;
 
-mongoose.connect(MONGODB_URI , {useNewUrlParser: true });
+mongoose.connect(MONGODB_URI);
 
 app.use(express.json());
 app.use(cors());
