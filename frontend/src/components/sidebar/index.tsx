e0b02630bc82a01b721c3d1c004fd871030450f3
teegoood <teegooodgood@gmail.com>
2024-01-05 14:57:53 +0700

    feat: fix sidebar ui

--- frontend/src/components/sidebar/index.tsx
+++ frontend/src/components/sidebar/index.tsx
@@ -14,7 +14,8 @@ const SideBar = (
     const [students, setStudents] = useState<student[]>();
     const {id} = useParams();
     const [keyword, setKeyword] = useState("");
-    console.log(import.meta.env.API_DOMAIN);
+    
+    console.log(`api domain : ${import.meta.env.VITE_API_DOMAIN}`);
 
     async function fetch(url:string) {
         const response = await axios.get(url);
@@ -31,45 +32,41 @@ const SideBar = (
 
     return (  
         <div className="w-64 px-7 py-5 border-r border-r-primary-light">
-            {students &&
-                <>
-                    <h1 className="text-3xl font-bold mb-7 flex items-center gap-3">
-                        <span className="text-primary-normal">Zhuang</span>
-                        <Icon 
-                            icon="ph:student" color="#3559E0" 
-                            width={30} 
-                            height={30}/>
-                    </h1>
-                    <SearchBar setKeyword={setKeyword}/>
-                    
-                    <div className="h-1/2 -overflow-hidden mt-5">
-                        <button 
-                            className="bg-primary-normal text-white px-2 py-1 rounded-full hover:bg-primary-notThatLight transition flex gap-2 items-center mb-2" 
-                            onClick={() => setMode(Mode.STUDENT)}
-                        >
-                            <span >
-                                เพิ่มนักเรียน
-                            </span>
-                            <Icon icon="typcn:plus" color="white" />
-                        </button>
-                        {
-                            students.map((student) => {
-                                const studentId = student._id;
-                                const isActive = (id === studentId);
-                                if(!student.username.includes(keyword)){
-                                    return;
-                                }
-
-                                return (
-                                    <Link to={`/students/${studentId}`} key={studentId}>
-                                        <NameCard name={student.username} isActive={isActive} />
-                                    </Link>
-                                );
-                            })
+            <h1 className="text-3xl font-bold mb-7 flex items-center gap-3">
+                <span className="text-primary-normal">Zhuang</span>
+                <Icon 
+                    icon="ph:student" color="#3559E0" 
+                    width={30} 
+                    height={30}/>
+            </h1>
+            <SearchBar setKeyword={setKeyword}/>
+            
+            <div className="h-1/2 -overflow-hidden mt-5">
+                <button 
+                    className="bg-primary-normal text-white px-2 py-1 rounded-full hover:bg-primary-notThatLight transition flex gap-2 items-center mb-2" 
+                    onClick={() => setMode(Mode.STUDENT)}
+                >
+                    <span >
+                        เพิ่มนักเรียน
+                    </span>
+                    <Icon icon="typcn:plus" color="white" />
+                </button>
+                {students &&
+                    students.map((student) => {
+                        const studentId = student._id;
+                        const isActive = (id === studentId);
+                        if(!student.username.includes(keyword)){
+                            return;
                         }
-                    </div>
-                </>
-            }
+
+                        return (
+                            <Link to={`/students/${studentId}`} key={studentId}>
+                                <NameCard name={student.username} isActive={isActive} />
+                            </Link>
+                        );
+                    })
+                }
+            </div>
         </div>
     );
 }
